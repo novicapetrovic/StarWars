12aaba2447e238443ad9bbbdf1fb2b31d6fcd2a7
Nov Petrović <novicapetrovic97@gmail.com>
2023-01-16 13:59:07 +0000

    Initial Commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Nov Petrović on 16/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
