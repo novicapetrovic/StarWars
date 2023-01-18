a729233d71ec8d4e83b49876ee97865a31c71f0d
Nov Petrović <novicapetrovic97@gmail.com>
2023-01-18 20:09:46 +0000

    readding lost files

---
+++ StarWars/Networking/Model/CharacterModel.swift
@@ -0,0 +1,13 @@
+//
+//  StarWarsFilmDetailModel.swift
+//  StarWars
+//
+//  Created by Nov Petrović on 16/01/2023.
+//
+
+import Foundation
+
+struct CharacterModel: Codable {
+    let name: String
+    let url: String
+}
